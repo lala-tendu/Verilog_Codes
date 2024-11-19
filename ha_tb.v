97f48318249e4e54242ab418f94c8f821ae2ea4e
Lalatendu Tripathy <153801794+lala-tendu@users.noreply.github.com>
2024-11-19 20:59:37 +0530

    ha_tb.v

--- ha_tb.v
+++ ha_tb.v
@@ -1,12 +1,8 @@
 `timescale 1ns / 1ps
 
 module half_adder_tb;
-    
-    // Inputs
     reg a;
     reg b;
-    
-    // Outputs
     wire sum;
     wire carry;
     
@@ -18,25 +14,25 @@ module half_adder_tb;
         .carry(carry)
     );
     
-    // Test stimulus
+    
     initial begin
-        // Monitor the outputs
+        
         $monitor("a = %b, b = %b | sum = %b, carry = %b", a, b, sum, carry);
         
-        // Test case 1
-        a = 0; b = 0; // 0 + 0 = 00
+        
+        a = 0; b = 0; 
         #10;
         
-        // Test case 2
-        a = 0; b = 1; // 0 + 1 = 01
+        
+        a = 0; b = 1; 
         #10;
         
-        // Test case 3
-        a = 1; b = 0; // 1 + 0 = 01
+        
+        a = 1; b = 0; 
         #10;
         
-        // Test case 4
-        a = 1; b = 1; // 1 + 1 = 10
+        
+        a = 1; b = 1; 
         #10;
         
         $finish;
